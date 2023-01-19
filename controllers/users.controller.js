f7809350214b5862182cb5fa9afb7e92089483eb
Hashim-47 <hashim-47@Ubuntu.myguest.virtualbox.org>
2023-01-19 16:29:10 +0000

    Task 9 GET Users

---
+++ controllers/users.controller.js
@@ -0,0 +1,11 @@
+const {fetchUsers} = require('../models/users.model')
+
+const getUsers = (req, res, next) => {
+    fetchUsers().then((users) => {
+        res.status(200).send({users: users});
+    }).catch((err) => {
+        next(err)
+    })
+}
+
+module.exports = {getUsers} 
